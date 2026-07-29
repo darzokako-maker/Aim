cafadb9e3100f60e0198f7f3b5743e6918004d13
darzokako-maker <darzokako@gmail.com>
2026-07-29 22:50:26 +0300

    Refactor syscall execution comments for clarity

--- syscall_stub.asm
+++ syscall_stub.asm
@@ -4,10 +4,9 @@ EXTERN g_ssn : DWORD
 EXTERN g_syscall_gadget : QWORD
 
 ExecuteIndirectSyscall PROC
-    mov r10, rcx                    ; Windows x64 ABI gereği ilk parametre (cInputs) R10'a alınır
-    mov eax, g_ssn                  ; SSN numarası EAX yazmacına yüklenir
-    jmp qword ptr [g_syscall_gadget] ; win32u.dll içindeki meşru syscall gadget adresine zıplar
+    mov r10, rcx                    ; Windows x64 Syscall ABI: 1. parametre R10'a
+    mov eax, g_ssn                  ; SSN numarası EAX yazmacına
+    jmp qword ptr [g_syscall_gadget] ; win32u.dll içindeki meşru 'syscall; ret' adresine zıpla
 ExecuteIndirectSyscall ENDP
 
 END
-
