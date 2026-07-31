99605481226e36a490ea12f4fd51e49086bb1a86
darzokako-maker <darzokako@gmail.com>
2026-07-31 17:48:44 +0300

    Introduce junk code for obfuscation in syscall_stub.asm
    Added junk code blocks for obfuscation in syscall_stub.asm.

--- syscall_stub.asm
+++ syscall_stub.asm
@@ -4,13 +4,36 @@ EXTERN g_ssn : DWORD
 EXTERN g_syscall_gadget : QWORD
 
 ExecuteIndirectSyscall PROC
-    ; Windows x64 ABI: İlk parametre (cInputs) R10'a taşınır
+    ; --- JUNK CODE BLOCK 1 (Junk Register Manipulations) ---
+    push rax
+    push r11
+    xor rax, rax
+    add rax, 01337h
+    sub rax, 01337h
+    pop r11
+    pop rax
+    ; --------------------------------------------------------
+
+    ; Orijinal İşlem: Windows x64 ABI gereği ilk parametre (cInputs) R10'a alınır
     mov r10, rcx
     
+    ; --- JUNK CODE BLOCK 2 (Control Flow Flattening / Dead Branching) ---
+    test r10, r10
+    jnz VALID_EXEC
+    nop
+    nop
+    xchg r11, r11
+
+VALID_EXEC:
     ; SSN numarası EAX'e yüklenir
     mov eax, g_ssn
     
-    ; win32u.dll içerisindeki "syscall; ret" adresine zıpla
+    ; --- JUNK CODE BLOCK 3 ---
+    nop
+    nop
+    ; -------------------------
+
+    ; win32u.dll içerisindeki "syscall; ret" gadget adresine zıpla
     jmp qword ptr [g_syscall_gadget]
 ExecuteIndirectSyscall ENDP
 
